acf0eb5f7dc41e010b164fb27dd80a7ce02602b0
KrimsN <deni.tim99@yandex.ru>
2020-10-20 17:53:52 +0700

    new version

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='TestPack',
-      version='0.0.1',
+      version='0.0.2',
       description='My test python package',
       author='KrimsN',
       author_email='dr.krimsn@gmail.com',
